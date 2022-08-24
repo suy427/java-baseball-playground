4aac840d3efb1992ac11a9e6762ce597081ddcde
jy-son <jy.son.david@webtoonscorp.com>
2022-08-24 16:55:34 +0900

    String 클래스에 대한 학습 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,44 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split() {
+        String[] actual = "1,2".split(",");
+        assertThat(actual).contains("1");
+        assertThat(actual).contains("2");
+        assertThat(actual).containsExactly("1", "2");
+    }
+
+    @Test
+    void substring() {
+        String actual = "(1,2)".substring(1,4);
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt은 문자열의 특정 위치의 문자를 가져올 수 있다")
+    void chatAt() {
+        char actual = "abc".charAt(1);
+        assertThat(actual).isEqualTo('b');
+    }
+
+    @Test
+    @DisplayName("charAt은 문자열 길이를 초과하는 index값을 받았을 때 StringIndexOutOfBoundsException을 발생시킨다")
+    void chatAt_StringIndexOutOfBoundsException() {
+        assertThatThrownBy(() -> {
+            "abc".charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 }
